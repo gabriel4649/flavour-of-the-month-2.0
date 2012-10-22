a7677c29824ddb35f4a1935d4c007e28e28817ee
wing <winglok@gatech.edu>
2012-10-22 16:02:33 -0400

    improved vote, request views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.*;
 import play.mvc.*;
 import play.data.*;
@@ -8,83 +11,102 @@ import models.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
-    // -- Authentication
-    
-    public static class Login {
-        
-        public String email;
-        public String password;
-        
-        public String validate() {
-            if(User.authenticate(email, password) == null) {
-                return "Invalid user or password";
-            }
-            return null;
-        }
-        
-    }
 
-    /**
-     * Login page.
-     */
-    public static Result login() {
-        return ok(
-            login.render(form(Login.class))
-        );
-    }
-    
-    /**
-     * Handle login form submission.
-     */
-    public static Result authenticate() {
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        if(loginForm.hasErrors()) {
-            return badRequest(login.render(loginForm));
-        } else {
-            session("email", loginForm.get().email);
-            return redirect(
-                routes.Application.index()
-            );
-        }
-    }
+	// -- Authentication
 
-    /**
-     * Logout and clean the session.
-     */
-    public static Result logout() {
-        session().clear();
-        flash("success", "You've been logged out");
-        return redirect(
-            routes.Application.login()
-        );
-    }
-    
-    // -- For testing only, will be removed later
-    
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-      }
+	public static class Login {
 
-    public static Result requests() {
-    	    return ok(requests.render(User.findAll()));
-    	  }
-      
-    public static Result chat() {
-    	    return ok(chat.render("Your new application is ready."));
-    	  }
-      
-    public static Result vote() {
-    	    return ok(vote.render(User.findAll()));
-    	  }
-      
-    public static Result getMatches() {
-    	long id = 1;    
-    	return ok(viewMatches.render(Project.findById(id)));
-    	  }
-      
-    public static Result myProfile() {
-    	    return ok(myProfile.render("Your new application is ready."));
-    	  }
+		public String email;
+		public String password;
+
+		public String validate() {
+			if(User.authenticate(email, password) == null) {
+				return "Invalid user or password";
+			}
+			return null;
+		}
+
+	}
+
+	/**
+	 * Login page.
+	 */
+	public static Result login() {
+		return ok(
+				login.render(form(Login.class))
+				);
+	}
+
+	/**
+	 * Handle login form submission.
+	 */
+	public static Result authenticate() {
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if(loginForm.hasErrors()) {
+			return badRequest(login.render(loginForm));
+		} else {
+			session("email", loginForm.get().email);
+			return redirect(
+					routes.Application.index()
+					);
+		}
+	}
+
+	/**
+	 * Logout and clean the session.
+	 */
+	public static Result logout() {
+		session().clear();
+		flash("success", "You've been logged out");
+		return redirect(
+				routes.Application.login()
+				);
+	}
+
+	// -- For testing only, will be removed later
+
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result requests() {
+		String user1 = "acooper@sample.com";
+		String user2 = "laldridge@sample.com";
+		String user3 = "mandersen@sample.com";
+		User u1 = User.findByEmail(user1);
+		User u2 = User.findByEmail(user1);
+		User u3 = User.findByEmail(user1);
+		List<User> list = new ArrayList<User>();
+		list.add(u1);
+		list.add(u2);
+		list.add(u3);
+		return ok(requests.render(list));
+	}
+
+	public static Result chat() {
+		return ok(chat.render("Your new application is ready."));
+	}
+
+	public static Result vote() {
+		long id = 1;
+		Project p = Project.findById(id);
+		List<User> voteList = new ArrayList<User>();
+		voteList = p.members;
+		return ok(vote.render(voteList));
+	}
+
+	public static Result getMatches() {
+		long id = 1;
+		Project p = Project.findById(id);
+		List<User> matchesList = new ArrayList<User>();
+		matchesList = p.members;
+		return ok(viewMatches.render(matchesList));
+	}
+
+	public static Result myProfile() {
+		String user1 = "edilbert@sample.com";
+		User u1 = User.findByEmail(user1);
+		return ok(myProfile.render(u1));
+	}
 
 }
